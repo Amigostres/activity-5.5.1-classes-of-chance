4e4ba4fce50dacb7def007882e94cfd981acd197
Jose <josemanuel21302@gmail.com>
2023-01-26 19:29:08 -0800

    completed LOL

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 class Casino {
     // Write code here
+    constructor(name, isFakeCoin, timesPlayed){   
+        this.name = name;
+        this.timesPlayed = 0
+        this.isFakeCoin = isFakeCoin
+    }
+
+    playGame(num){
+        let roll = Math.random();
+        if(roll < 0.5 && this.isFakeCoin){
+            console.log(`you won ${num}!`);
+            console.log(`you've played ${this.timesPlayed++} games`)
+        }else{
+            console.log(`HackerU Casino wins!`);
+            console.log(`you've played ${this.timesPlayed++} games`)
+        }
+    }
+
+    rollDie(num){
+        let roll = Math.random(1/6)
+        console.log(`you rolled a ${Math.round(roll*10)}`)
+    }
+
 };
 
 // TESTS
@@ -11,20 +33,19 @@ myCasino.playGame(25);
 myCasino.playGame(35);
 
 // BONUS TESTS
-/*
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
 console.log(myBonusCasino);
 myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
 myBonusCasino.playGame(25);
 myBonusCasino.playGame(35);
-*/
+
 
 // Extra BONUS TESTS
-/*
+
 const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
 console.log(myExtraBonusCasino);
 myExtraBonusCasino.rollDie(6);
 myExtraBonusCasino.rollDie(20);
 myExtraBonusCasino.rollDie(100);
-*/
+
